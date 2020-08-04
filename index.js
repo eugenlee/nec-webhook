b73aa8fdda9c56e76c93d41fbfbb086d10a152d7
eugenl <eugenelukelee@gmail.com>
2020-08-04 00:16:09 -0700

    travels

--- index.js
+++ index.js
@@ -88,76 +88,64 @@ function handleMessage(sender_psid, received_message) {
   // Check if the message contains text
 
   if (payload) {
-      // First branch with website tabs
-      if (payload === 'WEBSITE') {
-        response = {
-          "text": "Which site would you like to view?",
-          "quick_replies":[
-            {
-              "content_type":"text",
-              "title":"Travels",
-              "payload": 'TRAVELS'
-            },
-            {
-              "content_type":"text",
-              "title":"Guides",
-              "payload": 'GUIDES'
-            },
-            {
-              "content_type":"text",
-              "title":"Services",
-              "payload": 'SERVICES'
-            },
-            {
-              "content_type":"text",
-              "title":"About Me",
-              "payload": 'ABOUT'
-            }
-          ]
-        }  
+    // FIRST BRANCH: website tabs
+    if (payload === 'WEBSITE') {
+      response = {
+        "text": "Which site would you like to view?",
+        "quick_replies":[
+          {
+            "content_type":"text",
+            "title":"Travels",
+            "payload": 'TRAVELS'
+          },
+          {
+            "content_type":"text",
+            "title":"Guides",
+            "payload": 'GUIDES'
+          },
+          {
+            "content_type":"text",
+            "title":"Services",
+            "payload": 'SERVICES'
+          },
+          {
+            "content_type":"text",
+            "title":"About Me",
+            "payload": 'ABOUT'
+          }
+        ]
+      }  
     }
-    // Talk to Jason Branch
+    // talk to jason 
     else if (payload === 'TALK') {
       response = { "text": "Jason will be with you shortly!" }
     }
   }
-  else if (received_message.text) {    
+  // SECOND BRANCH: travels
+  else if (payload === 'TRAVELS') {
+    response = {
+      "type": "web_url",
+      "url": "https://www.neverendingcycle.org/travels.html",
+      "title": "Check out my recent Travels!"
+    }
+  }
+  // guides
+  else if (payload === 'GUIDES') {
+    response = {
+      
+    }
+  }
+  // services
+  else if (payload === 'SERVICES') {
+    response = {
+      
+    }
+  }  // about me
+  else if (payload === 'ABOUT') {
+    response = {
+      
+    }
 
-    // Create the payload for a basic text message
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  } else if (received_message.attachments) {
-  
-    // Gets the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
-  }   
-  
   // Sends the response message
   callSendAPI(sender_psid, response);  
     
