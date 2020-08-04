4c8989de0cc55fb6d2d60d6625d5a6ee0c168801
eugenl <eugenelukelee@gmail.com>
2020-08-04 15:37:44 -0700

    ungh

--- index.js
+++ index.js
@@ -85,7 +85,6 @@ function handleMessage(sender_psid, received_message) {
   let response;
 
   var payload = received_message.quick_reply.payload;
-  // Check if the message contains text
 
   // FIRST BRANCH: website tabs
   if (payload === 'WEBSITE') {
@@ -209,6 +208,41 @@ function handleMessage(sender_psid, received_message) {
       }
     }
   }
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
   else {
     response = { "text": "Jason will be with you shortly!" }
 
@@ -246,6 +280,11 @@ function handlePostback(sender_psid, received_postback) {
         }
       ]
     }
+  }
+  else if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
   }
 
   // Send the message to acknowledge the postback
